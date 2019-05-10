27b5378e123374c933d910682921d81147eb2f2c
NK2448 <nikonufrienko@gmail.com>
2019-05-10 11:14:13 +0300

    Update mySerial.h

--- library/mySerial.h
+++ library/mySerial.h
@@ -11,10 +11,10 @@
 #include <avr/io.h>
 #include <math.h>
 #include <stdlib.h>
+
 void uartInit()
 {
-	//9600 бод
-	UBRR0L=108;//устнанавливаем скорость 9600
+	UBRR0L=8;//115200
 	UCSR0B |= (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);
 	UCSR0A |= (1 << U2X0);
 	UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00);
@@ -38,18 +38,18 @@ void writeint(long int d)
 {
 	if(d<0) send_byte('-');
 	else if(d==0) send_byte('0');
-	unsigned int buff=abs(d);
-	uint8_t lenin=0;
-	while(buff!=0)
+	long int buff= labs( d );
+	int lenin=1;
+	while(buff>=10)
 	{
 		buff/=10;
-		lenin++;
+		lenin*=10;
 	}
-	buff=abs(d);
-	for(uint8_t i=1; i <= lenin; i++)
+	buff=labs(d);
+	for(long int i=1; i <= lenin; i*=10)
 	{
-		send_byte(((unsigned char)(buff / pow(10, lenin - i))) + 0x30);
-		buff %= (long int)pow(10, lenin - i);
+		send_byte((uint8_t) (buff/(lenin/i)) + '\0'); 
+		buff %= lenin/i;
 	}
 }
 #endif /* MYSERIAL_H_ */
